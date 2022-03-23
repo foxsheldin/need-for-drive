4ff439f7213f4e095c218bdff86e2587b1b5df19
foxsheldin <fruktsky@gmail.com>
2022-03-23 23:46:27 +0300

    Рефакторинг кода в SliderContent

--- src/components/MainPage/Content/Slider/SliderContent.js
+++ src/components/MainPage/Content/Slider/SliderContent.js
@@ -7,6 +7,60 @@ import "swiper/css/pagination";
 import "./styles.scss";
 
 const SliderContent = () => {
+  const slidesData = [
+    {
+      bgStyles: "slide-1",
+      header: "Бесплатная парковка",
+      info: "Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах.",
+      btnStyles: "button_green",
+      btnActionName: "Подробнее",
+    },
+    {
+      bgStyles: "slide-2",
+      header: "Страховка",
+      info: "Полная страховка страховка автомобиля",
+      btnStyles: "button_blue",
+      btnActionName: "Подробнее",
+    },
+    {
+      bgStyles: "slide-3",
+      header: "Бензин",
+      info: "Полный бак на любой заправке города за наш счёт",
+      btnStyles: "button_orange",
+      btnActionName: "Подробнее",
+    },
+    {
+      bgStyles: "slide-4",
+      header: "Обслуживание",
+      info: "Автомобиль проходит еженедельное ТО",
+      btnStyles: "button_purple",
+      btnActionName: "Подробнее",
+    },
+  ];
+
+  const slides = slidesData.map((item) => {
+    return (
+      <SwiperSlide>
+        <div className={"slider__item " + item.bgStyles}>
+          <div className="slider__discribe">
+            <div className="slider__wrapper">
+              <div className="slider__header">{item.header}</div>
+              <div className="slider__info">{item.info}</div>
+              <div className="slider__action">
+                <a
+                  href="#"
+                  className={"button slider__button " + item.btnStyles}
+                >
+                  {item.btnActionName}
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+    );
+  });
+
   return (
     <Swiper
       cssMode={true}
@@ -20,75 +74,7 @@ const SliderContent = () => {
       modules={[Navigation, Pagination, Mousewheel, Keyboard]}
       className="mySwiper"
     >
-      <SwiperSlide>
-        <div className="slider__item slide-1">
-          <div className="slider__discribe">
-            <div className="slider__wrapper">
-              <div className="slider__header">Бесплатная парковка</div>
-              <div className="slider__info">
-                Оставляйте машину на платных городских парковках и разрешенных
-                местах, не нарушая ПДД, а также в аэропортах.
-              </div>
-              <div className="slider__action">
-                <a href="#" className="button slider__button button_green">
-                  Подробнее
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="slider__item slide-2">
-          <div className="slider__discribe">
-            <div className="slider__wrapper">
-              <div className="slider__header">Страховка</div>
-              <div className="slider__info">
-                Полная страховка страховка автомобиля
-              </div>
-              <div className="slider__action">
-                <a href="#" className="button slider__button button_blue">
-                  Подробнее
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="slider__item slide-3">
-          <div className="slider__discribe">
-            <div className="slider__wrapper">
-              <div className="slider__header">Бензин</div>
-              <div className="slider__info">
-                Полный бак на любой заправке города за наш счёт
-              </div>
-              <div className="slider__action">
-                <a href="#" className="button slider__button button_orange">
-                  Подробнее
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="slider__item slide-4">
-          <div className="slider__discribe">
-            <div className="slider__wrapper">
-              <div className="slider__header">Обслуживание</div>
-              <div className="slider__info">
-                Автомобиль проходит еженедельное ТО
-              </div>
-              <div className="slider__action">
-                <a href="#" className="button slider__button button_purple">
-                  Подробнее
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </SwiperSlide>
+      {slides}
     </Swiper>
   );
 };
