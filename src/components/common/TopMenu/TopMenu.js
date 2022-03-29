f76eafc33a2d8711204f1894f3f0ad2998439fa2
foxsheldin <fruktsky@gmail.com>
2022-03-29 21:59:44 +0300

    V-01 Фикс стилей TopMenu

--- src/components/common/TopMenu/TopMenu.js
+++ src/components/common/TopMenu/TopMenu.js
@@ -4,11 +4,9 @@ import locationLogo from "../../../assets/images/location.svg";
 
 const TopMenu = () => {
   return (
-    <div className="main-container__place">
+    <div className="main-container__place place">
       <div className="place__logo">Need for drive</div>
-      <div className="place__location">
-        <span className="location__name">Ульяновск</span>
-      </div>
+      <div className="place__location">Ульяновск</div>
     </div>
   );
 };
